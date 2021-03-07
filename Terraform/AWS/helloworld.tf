6331fe9f87c02ac7a1ce269bee11a19d3c14e446
Amit Sharma <amitksit83@gmail.com>
2021-03-07 11:07:14 +0530

    edit instance resources

--- Terraform/AWS/helloworld.tf
+++ Terraform/AWS/helloworld.tf
@@ -3,12 +3,18 @@ provider "aws" {
   profile = "default"
   shared_credentials_file = "~/.aws/credentials"
 }
-//This is a string variable
-variable "firststring" {
-  type = "string"
-  default = "This is my first string"
+
+resource "aws_instance" "my-first-server" {
+  ami           = "ami-0d758c1134823146a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Amit-Server"
+  }
 }
 
-output "myfirstoutput" {
-  value = "var.firststring"
-}
+/* resource "<provider>_<resource_type>" "name" {
+  config option....
+  key = "value"
+  key2 = "value"
+} */
