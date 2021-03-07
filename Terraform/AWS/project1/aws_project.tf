e669181dbf1b2594fee608bfa5ff95b4450d5acc
Amit Sharma <amitksit83@gmail.com>
2021-03-07 17:28:28 +0530

    Add Cidr_block ""

--- Terraform/AWS/project1/aws_project.tf
+++ Terraform/AWS/project1/aws_project.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "prod_allow_web" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = 0.0.0.0/0
+    cidr_blocks = "0.0.0.0/0"
   }
 
   ingress {
@@ -68,14 +68,14 @@ resource "aws_security_group" "prod_allow_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = 0.0.0.0/0
+    cidr_blocks = "0.0.0.0/0"
   }  
   ingress {
     description = "SSH"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = 0.0.0.0/0
+    cidr_blocks = "0.0.0.0/0"
   }
   egress {
     from_port   = 0
