cf3c88d4aa4858600ba2e6d494d0437ffd604574
Amit Sharma <amitksit83@gmail.com>
2021-03-07 15:38:42 +0530

    Change VPC and Subnet Name

--- Terraform/AWS/vpc/aws_vpc.tf
+++ Terraform/AWS/vpc/aws_vpc.tf
@@ -4,15 +4,15 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "first_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "prod_vpc"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
+resource "aws_subnet" "first_subnet" {
+  vpc_id     = aws_vpc.first_vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -20,15 +20,15 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_vpc" "my_vpc1" {
+resource "aws_vpc" "second_vpc" {
   cidr_block = "10.1.0.0/16"
   tags = {
     Name = "dev_vpc"
   }
 }
 
-resource "aws_subnet" "my_subnet1" {
-  vpc_id     = aws_vpc.my_vpc1.id
+resource "aws_subnet" "second_subnet" {
+  vpc_id     = aws_vpc.second_vpc.id
   cidr_block = "10.1.1.0/24"
 
   tags = {
