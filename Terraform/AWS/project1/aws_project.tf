e83954e77e2ab985f652191ee7817d8fa23de393
Amit Sharma <amitksit83@gmail.com>
2021-03-07 17:44:20 +0530

    change ssh port

--- Terraform/AWS/project1/aws_project.tf
+++ Terraform/AWS/project1/aws_project.tf
@@ -72,8 +72,8 @@ resource "aws_security_group" "prod_allow_web" {
   }  
   ingress {
     description = "SSH"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
