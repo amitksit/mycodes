a33d1ef68015bdd4cf814690b5ac18a21db99b7b
Amit Sharma <amitksit83@gmail.com>
2021-03-07 15:28:54 +0530

    add second vpc and subnet

--- Terraform/AWS/vpc/aws_vpc.tf
+++ Terraform/AWS/vpc/aws_vpc.tf
@@ -19,3 +19,19 @@ resource "aws_subnet" "my_subnet" {
     Name = "prod_subnet"
   }
 }
+
+resource "aws_vpc" "my_vpc1" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dev_vpc"
+  }
+}
+
+resource "aws_subnet" "my_subnet1" {
+  vpc_id     = aws_vpc.my_vpc1.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "dev_subnet"
+  }
+}
